0d6dd40b658b1f2080b52b364e9c630f7dc89981
Siwoo-Kim <skim327@myseneca.ca>
2018-01-27 13:02:40 -0500

    HIBERNATE_VERSION 1.0

--- src/test/java/com/hibernate/learning/domain/MemberTest.java
+++ src/test/java/com/hibernate/learning/domain/MemberTest.java
@@ -1,5 +1,6 @@
 package com.hibernate.learning.domain;
 
+import lombok.extern.java.Log;
 import org.junit.Test;
 
 import javax.persistence.EntityManager;
@@ -7,12 +8,19 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-import static org.junit.Assert.assertNotNull;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+@Log
 public class MemberTest {
 
-    @Test
-    public void persistTest(){
+    public static void strategy_context(Consumer<EntityManager> consumer){
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hibernate");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         EntityTransaction entityTransaction = entityManager.getTransaction();
@@ -21,9 +29,7 @@ public class MemberTest {
             entityTransaction.begin();
             assertNotNull(entityManager);
 
-            Member member = Member.builder().age(10).username("Siwoo").build();
-            entityManager.persist(member);
-
+            consumer.accept(entityManager);
 
             entityTransaction.commit();
 
@@ -35,4 +41,44 @@ public class MemberTest {
 
         entityManagerFactory.close();
     }
+
+    @Test
+    public void logic1(){
+
+        strategy_context((entityManager)->{
+
+            Member member = Member.builder()
+                    .username("Siwoo Kim")
+                    .age(28)
+                    .build();
+
+            entityManager.persist(member);
+
+            member.setAge(25);
+
+            Member foundMember = entityManager.find(Member.class,member.getId());
+
+            assertThat(foundMember,is(member));
+            assertThat(foundMember.getAge(),is(25));
+
+            log.warning(foundMember.toString());
+
+            List<Member> memberList = entityManager.createQuery("select m from Member m").getResultList();
+
+            assertThat(memberList.size(),is(1));
+
+            log.warning("Member Size "+memberList.size());
+
+            entityManager.remove(foundMember);
+
+            memberList = entityManager.createQuery("select m from Member m").getResultList();
+
+            assertThat(memberList.size(),is(0));
+
+            log.warning("Member Size "+memberList.size());
+
+
+        });
+    }
+
 }
